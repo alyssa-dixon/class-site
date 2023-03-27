3a85b1c8ff54a7cdb83a912b2ac0d488ae794d09
02alyssa <123617966+alyssa-dixon@users.noreply.github.com>
2023-03-27 10:58:55 -0400

    portfolio

---
+++ jquery/scripts.js
@@ -0,0 +1,22 @@
+$(".dark-btn").click(function(){
+    $(".box").toggleClass("dark");
+    $("body").toggleClass("dark");
+  });
+
+$(".glow-btn").click(function(){
+    $(".box").toggleClass("glow");
+    });
+
+$(".spin-btn").click(function(){
+    $(".box").toggleClass("spin");
+    });
+
+$(".reveal-btn").click(function(){
+    $(".graham-cracker").css({"opacity":"1"});
+
+    $(".reveal-btn").hide()
+    });
+
+$(".draggable").draggable();   
+
+$(".draggable").draggable({ snap: true }); 
