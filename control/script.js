0f957ea769fb8c9a5a981348542e90f088eb3cfa
02alyssa <123617966+alyssa-dixon@users.noreply.github.com>
2023-03-28 23:40:55 -0400

    control & main page

---
+++ control/script.js
@@ -0,0 +1,39 @@
+$(".draggable").draggable();   
+
+$(".draggable").draggable({ snap: true }); 
+
+$(".blue").click(function(){
+    $(".new-blue").css({"opacity":"1"});
+});
+
+$(".violet").click(function(){
+    $(".new-violet").css({"opacity":"1"});
+});
+
+$(".purple").click(function(){
+    $(".new-purple").css({"opacity":"1"});
+});
+
+$(".light-pink").click(function(){
+    $(".new-light-pink").css({"opacity":"1"});
+});
+
+$(".orange").click(function(){
+    $(".new-orange").css({"opacity":"1"});
+});
+
+$(".grain").click(function(){
+    $(".new-grain").css({"opacity":"1"});
+});
+
+$(".yellow").click(function(){
+    $(".new-yellow").css({"opacity":"1"});
+});
+
+$(".tan").click(function(){
+    $(".new-tan").css({"opacity":"1"});
+});
+
+$(".multi-color").click(function(){
+    $(".new-multi-color").css({"opacity":"1"});
+});
