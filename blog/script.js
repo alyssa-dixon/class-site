e061e7bea8ded879a10a4db8c950316015c6eaca
02alyssa <123617966+alyssa-dixon@users.noreply.github.com>
2023-04-26 00:00:59 -0400

    blog updates

--- blog/script.js
+++ blog/script.js
@@ -1,2 +1,11 @@
 $( ".draggable" ).draggable();
 
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "200px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
